5d6208c92614d4f157a6cec81904d754a2903694
nir sivan <nirsivan204@gmail.com>
2022-05-09 14:00:03 +0300

    first commit

---
+++ Assets/scripts/MusicMGR.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMGR : MonoBehaviour
+{
+    [Serializable]
+    public class SoundType_And_Ref
+    {
+        public SoundTypes SoundType;
+        public AudioClip AudioClipRef;
+    }
+
+    [Serializable]
+    public class SoundSource_And_Ref
+    {
+        public AudioSourceTypes SourceType;
+        public AudioSource AudioSourceRef;
+    }
+
+
+    [SerializeField]
+    private List<SoundType_And_Ref> SoundType_And_Ref_List = new List<SoundType_And_Ref>();
+
+    [SerializeField]
+    private List<SoundSource_And_Ref> SoundSource_And_Ref_List = new List<SoundSource_And_Ref>();
+
+
+    public enum SoundTypes
+    {
+        None = 0,
+
+        mixer = 1,
+
+        // UI_Sounds
+        Click_01 = 100,
+
+        // Gameplay Sounds
+
+        // Music
+        BG_Music_1 = 300,
+        BG_Music_2 = 301,
+        //other
+    }
+
+    public enum AudioSourceTypes
+    {
+        None,
+        UI,
+        Gameplay,
+        Music,
+    }
+
+    internal void Init()
+    {
+
+    }
+
+    public void Play_Sound(SoundTypes soundType, bool isLoop = false)
+    {
+        AudioClip clip = Get_AudioClip_Of(soundType);
+
+        AudioSource source = Get_AudioSource_Of(soundType);
+
+        source.loop = isLoop;
+
+        Play_Sound(clip, source);
+
+    }
+
+    private void Play_Sound(AudioClip clip, AudioSource source)
+    {
+        source.clip = clip;
+        source.pitch = 1;
+
+        source.Play();
+    }
+
+
+    private AudioSource Get_AudioSource_Of(SoundTypes soundType)
+    {
+        switch (soundType)
+        {
+            case SoundTypes.None:
+                break;
+            case SoundTypes.Click_01:
+                return Get_AudioSource_By_Type(AudioSourceTypes.UI);
+            case SoundTypes.BG_Music_1:
+            case SoundTypes.BG_Music_2:
+                return Get_AudioSource_By_Type(AudioSourceTypes.Music);
+            default:
+                return Get_AudioSource_By_Type(AudioSourceTypes.Gameplay);
+        }
+
+        return null;
+    }
+
+    private AudioSource Get_AudioSource_By_Type(AudioSourceTypes audioSourceType)
+    {
+        for (int i = 0; i < SoundSource_And_Ref_List.Count; i++)
+        {
+            if (SoundSource_And_Ref_List[i].SourceType == audioSourceType)
+            {
+                return SoundSource_And_Ref_List[i].AudioSourceRef;
+            }
+        }
+
+        return null;
+    }
+
+    private AudioClip Get_AudioClip_Of(SoundTypes soundType)
+    {
+        for (int i = 0; i < SoundType_And_Ref_List.Count; i++)
+        {
+            if (SoundType_And_Ref_List[i].SoundType == soundType)
+                return SoundType_And_Ref_List[i].AudioClipRef;
+        }
+
+        return null;
+    }
+
+    public void AddPitch(SoundTypes soundType, float addedPitch)
+    {
+        AudioSource source = Get_AudioSource_Of(soundType);
+        source.pitch += addedPitch;
+    }
+}
---
+++ Assets/scripts/ParticleMGR.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMGR : MonoBehaviour
+{
+    [Serializable]
+    public class ParticleType_And_Ref
+    {
+        public ParticleTypes ParticleType;
+        public GameObject particleRef;
+    }
+
+    [SerializeField]
+    private List<ParticleType_And_Ref> ParticleType_And_Ref_List = new List<ParticleType_And_Ref>();
+
+
+    public enum ParticleTypes
+    {
+        None,
+        tornado
+
+    }
+
+    internal void Init()
+    {
+
+    }
+
+    public GameObject Play_Effect(ParticleTypes particleType, Vector3 pos, Transform parent = null)
+    {
+        GameObject particle = getParticleSystemRef(particleType);
+        return Play_Effect(particle, pos, parent);
+
+    }
+
+    private GameObject Play_Effect(GameObject particle, Vector3 position, Transform parent)
+    {
+        if (!parent)
+        {
+            parent = transform;
+        }
+        GameObject clone = Instantiate(particle, parent);
+        clone.transform.localPosition = position;
+        clone.GetComponent<ParticleSystem>().Play();
+        return clone;
+    }
+
+    private GameObject getParticleSystemRef(ParticleTypes particleType)
+    {
+        for (int i = 0; i < ParticleType_And_Ref_List.Count; i++)
+        {
+            if (ParticleType_And_Ref_List[i].ParticleType == particleType)
+                return ParticleType_And_Ref_List[i].particleRef;
+        }
+
+        return null;
+    }
+}
